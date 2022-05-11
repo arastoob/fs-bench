6ba95512445d9a57ddd570eb1897fdb35144be54
Arastoo Bozorgi <a.bozorgi67@gmail.com>
2022-05-11 11:17:08 -0230

    Add minute and hour formats as well

--- src/format.rs
+++ src/format.rs
@@ -8,8 +8,17 @@ pub fn time_format(s: f64) -> String {
     } else if s * 1000f64 < 1000f64 {
         let millis = milli_second(s);
         format!("{} ms", millis)
+    } else if s < 60f64 {
+        format!("{} s", second(s))
+    } else if s < 3600f64 {
+        let min = (s / 60f64) as i64;
+        let second = second(s % 60f64);
+        format!("{}:{}", min, second)
     } else {
-        format!("{} s", s)
+        let hour = (s / 3600f64) as i64;
+        let min = ((s % 3600f64) / 60f64) as i64;
+        let second = second(s % 60f64);
+        format!("{}:{}:{}", hour, min, second)
     }
 }
 
@@ -31,6 +40,11 @@ fn nano_second(s: f64) -> f64 {
     (nano * 1e4).trunc() / 1e4
 }
 
+fn second(s: f64) -> f64 {
+    // output to 4 floating points
+    (s * 1e4).trunc() / 1e4
+}
+
 #[cfg(test)]
 mod test {
     use crate::format::time_format;
@@ -46,6 +60,9 @@ mod test {
         assert_eq!(time_format(0.0012587), "1.2587 ms".to_string());
         assert_eq!(time_format(0.012587), "12.587 ms".to_string());
         assert_eq!(time_format(0.12587), "125.87 ms".to_string());
-        assert_eq!(time_format(12587.0), "12587 s".to_string());
+        assert_eq!(time_format(1258.71542645), "20:58.7154".to_string());
+
+        assert_eq!(time_format(65.126543), "1:5.1265".to_string());
+        assert_eq!(time_format(3601.15236), "1:0:1.1523".to_string());
     }
 }
